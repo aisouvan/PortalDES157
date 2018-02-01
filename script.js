c2c742cd299d0b133ea2d9169b33286b28f2f10f
Anatta Souvannarath <aisouvan@scc2102-6.ucdavis.edu>
2018-02-01 15:24:08 -0800

    took out break

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ function processForm() {
   var myMsg = document.getElementById("myMsg");
 
   myMsg.innerHTML =
-    "<br>"
-  "Dear " + jobTitle + ", " + "<br>" + " I am very interested in the" + workField + "Department during the" + season + "Quarter.I am a" + gradeLevel + "at the University of" + state + "," + city + ", pursuing a bachelor’s degree in" + major + "with a minor in" + major + "." + "My research experience has" + adj + "my interest in the way that" +
+
+    "Dear " + jobTitle + ", " + "<br>" + " I am very interested in the" + workField + "Department during the" + season + "Quarter.I am a" + gradeLevel + "at the University of" + state + "," + city + ", pursuing a bachelor’s degree in" + major + "with a minor in" + major + "." + "My research experience has" + adj + "my interest in the way that" +
     noun + "is delivered through" + nounOne + "and" + nounTwo + "." + "I believe in the mission of" + company + "and would be excited to be an intern with your organization. </p>" + "<br>" + "I am confident that my experiences, skills and strong interests in the goals of" + famousCo +
     "have prepared me to be a strong intern for your program. I have attached my resume and look forward to discussing my qualifications with you. Thank you for your time and consideration." + "<br>" + "Sincerely," + yourName;
   return false;
